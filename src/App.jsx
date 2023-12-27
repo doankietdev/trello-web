02f892d6dafc71bd2b211abd52c7f3a76d039e52
doananhkiet886 <doananhkiet886@gmail.com>
2023-12-27 16:46:02 +0700

    install mui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+      <p>Doan Anh Kiet</p>
+     <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
