d47f0564ba1ca633434eca38f5327a37ed91bdea
doananhkiet886 <doananhkiet886@gmail.com>
2023-12-28 00:28:22 +0700

    config dark light mode

---
+++ src/ModeToggle.jsx
@@ -0,0 +1,17 @@
+import { useColorScheme } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+export default ModeToggle
