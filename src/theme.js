2bd5323993a6ae05ced3e5ff776b4c651399d215
doananhkiet886 <doananhkiet886@gmail.com>
2024-01-02 15:54:44 +0700

    create basic layout

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,10 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
