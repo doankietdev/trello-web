214d40b3fb09ca36135eee7d9a06a3212f5fe5cd
doananhkiet886 <doananhkiet886@gmail.com>
2024-01-07 15:18:30 +0700

    add board content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -39,7 +39,8 @@ function BoardBar() {
       paddingX: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      borderBottom: '1px solid white'
+      borderBottom: '1px solid white',
+      '&::-webkit-scrollbar-track': { m: '10px' }
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <Chip
@@ -107,7 +108,10 @@ function BoardBar() {
               width: 34,
               height: 34,
               fontSize: '1rem',
-              border: 'none'
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0de' }
             }
           }}
         >
